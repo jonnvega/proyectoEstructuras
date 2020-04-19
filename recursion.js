377f15595af8e3779e98175a65d9642b7124f748
jonnvega <47796239+jonnvega@users.noreply.github.com>
2020-04-19 12:47:29 -0600

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -49,7 +49,7 @@ function getData() {
 
         interes = capitalRec(capital,tiempo,tasa).toFixed(2)
         
-        balance = capital+parseFloat(interes) 
+        balance = parseFloat(capital)+parseFloat(interes) 
         
         document.getElementById("interes").innerHTML = "El interés es " + interes + " " + moneda;
         document.getElementById("balance").innerHTML = "El balance es " + balance + " " + moneda
